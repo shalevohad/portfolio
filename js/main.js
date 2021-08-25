3778973185eac2e5350cfd63b7c1e7fd9fe83758
OhadShalev <shalev.ohad@gmail.com>
2021-08-25 15:40:34 +0300

    update JS, Css and About Section

--- js/main.js
+++ js/main.js
@@ -4,6 +4,36 @@ $(function () {
 
 function main() {
   console.log('Portfolio page loaded!');
+
+  const navbar = document.getElementById("main-nav");
+  // const backToTop = document.getElementById("backToTop");
+  // $(backToTop).hide();
+
+  let scrolled = false;
+  window.onscroll = function () {
+    if (window.pageYOffset > 200) {
+      navbar.classList.remove('navbar-transparent');
+
+      // if (!scrolled) navbar.style.transform = 'translateY(-70px)';
+      // setTimeout(function () {
+      //   navbar.style.transform = 'translateY(0)';
+      //   scrolled = true;
+      // }, 200);
+
+    }
+    else {
+      navbar.classList.add('navbar-transparent');
+      scrolled = false;
+    }
+
+    // Back To Top Button
+    if (window.pageYOffset > window.innerHeight * 0.5) {
+      $(backToTop).show();
+    }
+    else {
+      $(backToTop).hide();
+    }
+  }
 }
 
 // Listeners
